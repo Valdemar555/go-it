34543be909fde866fd82cd5f74e0b59b3e9d8678
Vladimir Cherevko <anuris@ukr.net>
2022-02-22 13:13:01 +0200

    hw7

--- hw7/setup.py
+++ hw7/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name ="clean-folder",
-    version = "0.0.1",
-    entry_points = {
-        "console_scripts" : ["clean-folder=console_script.my_cod:my_func"],
-        },
-    packages = find_packages(),
+    name="clean-folder",
+    version="0.0.1",
+    entry_points={
+        "console_scripts": ["clean-folder=clean_folder.clean:my_func"],
+    },
+    packages=find_packages(),
 )
